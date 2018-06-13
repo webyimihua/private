896a671b52b1e39365977c5536454f3f074d8e41
fanjifei <1169276107@qq.com>
2018-06-13 22:04:01 +0800

    删除dist文件

--- 路桥/layer通用模板/dist/js/page/datail.js
+++
@@ -1,9 +0,0 @@
-var $ ;
-layui.use(['element','layer','jquery'],function(){
-	$ = layui.$;
-	var b_nums = $(".canvas-contai .bradge-items").length;
-	$(".canvas-contai").width(160*b_nums + "px");
-    $.get("../../json/detail.json",function(data){
-         console.log(data)
-	})
-})
