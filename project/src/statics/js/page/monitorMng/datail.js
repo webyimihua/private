aeded3373f9e304f9b6afbd6f3973c742e49f113
fanjifei <1169276107@qq.com>
2018-06-26 08:19:56 +0800

    修改

--- project/src/statics/js/page/monitorMng/datail.js
+++ project/src/statics/js/page/monitorMng/datail.js
@@ -3,7 +3,13 @@ layui.use(['element','layer','jquery'],function(){
 	$ = layui.$;
 	var b_nums = $(".canvas-contai .bradge-items").length;
 	$(".canvas-contai").width(160*b_nums + "px");
+	// $(".bradge-box").width(160*b_nums + "px");
     $.get("../../json/detail.json",function(data){
          console.log(data)
 	})
+
+	$(".close_btn").click(function(){
+    	var index = parent.layer.getFrameIndex(window.name);
+        parent.layer.close(index);
+    })
 })
