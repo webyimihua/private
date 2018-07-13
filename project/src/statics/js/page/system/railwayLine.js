1339fcf8cbb6671e0bfb6e756e26bde3f79d65b1
fanjifei <fanjf@yfh100.com>
2018-07-13 14:58:29 +0800

    修改铁路线

--- project/src/statics/js/page/system/railwayLine.js
+++ project/src/statics/js/page/system/railwayLine.js
@@ -18,7 +18,7 @@ layui.use(['form','layer','table','laytpl','tools'],function(){
         page : true,
         height : "full-125",
         limits : [10,15,20,25],
-        limit : 20,
+        limit :10,
         id : "itemListtable",
         method: 'post',
         where: {
@@ -34,14 +34,18 @@ layui.use(['form','layer','table','laytpl','tools'],function(){
           limitName: 'pageSize' //每页数据量的参数名，默认：limit
         },
         response: {
-        	
+    	   statusName: 'result' //数据状态的字段名称，默认：code
+		  ,statusCode: 1 //成功的状态码，默认：0
+		  ,msgName: 'message' //状态信息的字段名称，默认：msg
+		  ,countName: 'count' //数据总数的字段名称，默认：count
+		  ,dataName: 'data' //数据列表的字段名称，默认：data	
         },
         cols : [[
             // {type: "checkbox", fixed:"left", width:50},
             {field: 'index', title: '序号', width:80, align:"center",templet: function(d){
                 return d.LAY_TABLE_INDEX + 1;
             }},
-            {field: 'userName', title: '铁路线', minWidth:280, align:"center"},
+            {field: 'name', title: '铁路线', minWidth:280, align:"center"},
             {title: '操作', minWidth:175, templet:'#handleListBar',fixed:"right",align:"center"}
         ]]
     });
