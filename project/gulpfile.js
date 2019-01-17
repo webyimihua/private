a53f89fe89ede0df96bf06a89078c64f4c280db9
fanjifei <fanjf@yfh100.com>
2019-01-17 13:12:04 +0800

    修改

--- project/gulpfile.js
+++
@@ -1,4 +0,0 @@
-var prod = require('./config/gulpfile.prod.js');
-var dev = require('./config/gulpfile.dev.js');
-dev();
-prod();
--- project/src/statics/js/main.js
+++
@@ -1,7 +0,0 @@
-layui.use(['form','element','layer','jquery'],function(){
-    var form = layui.form,
-    layer = parent.layer === undefined ? layui.layer : top.layer,
-    element = layui.element;
-    $ = layui.jquery;
-    
-})
